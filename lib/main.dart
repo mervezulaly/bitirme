5e05b8ede2090df58c4b922953c1d9f496e21255
mervezulaly <119618611+mervezulaly@users.noreply.github.com>
2023-04-18 12:51:16 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bitirme_proje/firebase_options.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_bitirme_proje/pages/register.dart';
+
 // import 'package:flutter_bitirme_proje/pages/main.dart'
 
 void main() {
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mesajlaşma UYgulaması",
+      title: "Mesajlaşma Uygulaması",
       home: Login(),
     );
   }
@@ -23,23 +26,199 @@ class MyApp extends StatelessWidget {
 class Login extends StatelessWidget {
   const Login({super.key});
 
+  get userName => null;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(),
-            TextField(),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: ((context) => Temel())));
-                },
-                child: Text("Login"))
-          ],
+    // return Scaffold(
+    //   appBar: AppBar(title: Text("Login")),
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         TextField(),
+    //         TextField(),
+    //         ElevatedButton(
+    //             onPressed: () {
+    //               Navigator.push(context,
+    //                   MaterialPageRoute(builder: ((context) => Temel())));
+    //             },
+    //             child: Text("Login"))
+    //       ],
+    //     ),
+
+    //   ),
+    // );
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 241, 180, 101),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              Text("Giriş Yap"),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 70, right: 70),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      FaIcon(FontAwesomeIcons.google, color: Colors.black),
+                      SizedBox(width: 5),
+                      Text(
+                        "Google ile giriş yap",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Spacer()
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 70, right: 70, top: 10),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Spacer(),
+                      FaIcon(
+                        FontAwesomeIcons.apple,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        "Apple ile giriş yap",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      Spacer()
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Divider(
+                    indent: 70,
+                    thickness: 3,
+                    color: Colors.white,
+                    height: 20,
+                  )),
+                  SizedBox(width: 10),
+                  Text(
+                    "veya",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Divider(
+                      thickness: 3,
+                      endIndent: 70,
+                      height: 20,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 70, right: 70, top: 10),
+                child: TextField(
+                  controller: userName,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      labelText:
+                          "Telefon numarası, E-posta veya kullanıcı adı"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 70, right: 70),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                    onPressed: () {
+                      if (userName.text == User) {
+                        print("Giriş Başarılı");
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Temel()));
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: ((context) => Temel())));
+                          },
+                          child: Text(
+                            "İleri",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 70, right: 70),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Şifreni mi unuttun?"),
+                      ],
+                    )),
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text(
+                    "Henüz bir hesabın yok mu?",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => Register())));
+                      },
+                      child: Text("Kaydol"))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -50,7 +229,11 @@ class Temel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Mesajlaşma Uygulaması")),
+      backgroundColor: Color.fromARGB(255, 241, 180, 101),
+      appBar: AppBar(
+        title: Text("Mesajlaşma Uygulaması"),
+        backgroundColor: Colors.orange,
+      ),
       body: AnaEkran(),
     );
   }
@@ -128,6 +311,7 @@ class MesajBaloncuk extends StatelessWidget {
       child: Row(children: <Widget>[
         CircleAvatar(
           child: Text(isim[0]),
+          backgroundColor: Colors.greenAccent,
         ),
         Column(
           children: <Widget>[
